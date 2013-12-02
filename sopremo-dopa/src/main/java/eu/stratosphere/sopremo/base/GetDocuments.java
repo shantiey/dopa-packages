cf139d18cd2d5e9abac67b495fa0dd0739e9f1b1
hendrikheller <hendrik.heller@campus.tu-berlin.de>
2013-12-02 13:44:59 +0100

    some changes

--- sopremo-dopa/src/main/java/eu/stratosphere/sopremo/base/GetDocuments.java
+++ sopremo-dopa/src/main/java/eu/stratosphere/sopremo/base/GetDocuments.java
@@ -5,41 +5,43 @@ import eu.stratosphere.pact.common.stubs.MapStub;
 import eu.stratosphere.pact.common.type.PactRecord;
 import eu.stratosphere.pact.common.type.base.PactString;
 import eu.stratosphere.sopremo.expressions.ConstantExpression;
+import eu.stratosphere.sopremo.operator.ElementaryOperator;
 
-public class GetDocuments extends MapStub {
+public class GetDocuments extends ElementaryOperator<GetDocuments> {
 	
-	private PactString data_pool = new PactString();
-	private PactString identifier = new PactString();
-	private PactString meta_data = new PactString();
 	
-	private DataMarketAccess dm = new DataMarketAccess();
-	
-	private PactString content = new PactString();
-	
-	private PactRecord out = new PactRecord();
-	
-	@Override
-	public void map(PactRecord record, Collector<PactRecord> out)
-			throws Exception {
-		this.identifier = record.getField(0, PactString.class);
-		this.data_pool = record.getField(1, PactString.class);
-		this.meta_data = record.getField(2, PactString.class);
+
+	public static class Implementation extends MapStub {
 		
-		if(this.data_pool.toString().matches("DM")) {
-			//TODO: get content from DM
-			dm.setURLParameter(new ConstantExpression(this.identifier.toString()));
-			
-			
-		} else if (this.data_pool.toString().matches("IMR")) {
-			//TODO: get content from IMR
+		PactString data_pool = new PactString();
+		PactString identifier = new PactString();
+		PactString meta_data = new PactString();
+		
+		DataMarketAccess dm = new DataMarketAccess();
+		PactString content = new PactString();
+		
+		PactRecord out = new PactRecord();
+
+		@Override
+		public void map(PactRecord record, Collector<PactRecord> out)
+				throws Exception {
+			this.identifier = record.getField(0, PactString.class);
+			this.data_pool = record.getField(1, PactString.class);
+			this.meta_data = record.getField(2, PactString.class);
+
+			if(this.data_pool.toString().matches("DM")) {
+				//TODO: get content from DM
+				dm.setURLParameter(new ConstantExpression(this.identifier.toString()));
+
+
+			} else if (this.data_pool.toString().matches("IMR")) {
+				//TODO: get content from IMR
+			}
+
+			this.out.setField(0, this.identifier);
+			this.out.setField(1, this.content);
+
 		}
-		
-		this.out.setField(0, this.identifier);
-		this.out.setField(1, this.content);
-		
-	}
-	
-	
-	
 
+	}
 }
